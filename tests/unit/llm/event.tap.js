b1fccc2469e5dca2142ce5ed2c15515bf8ada001
James Sumners <jsumners@newrelic.com>
2024-01-05 10:51:23 -0500

    add event tests

--- tests/unit/llm/event.tap.js
+++ tests/unit/llm/event.tap.js
@@ -15,9 +15,22 @@ tap.beforeEach((t) => {
         return ['test-app']
       }
     },
-    llm: {
-      metadata: {
-        foo: 'foo'
+    tracer: {
+      getTransaction() {
+        return {
+          trace: {
+            custom: {
+              get(key) {
+                t.equal(key, 0x01 | 0x02 | 0x04 | 0x08)
+                return {
+                  attrs: {
+                    ['llm.conversation_id']: 'conversation-1'
+                  }
+                }
+              }
+            }
+          }
+        }
       }
     }
   }
@@ -26,29 +39,39 @@ tap.beforeEach((t) => {
     accessKeyId: '123456789'
   }
 
-  t.context.response = {
-    response: {
-      headers: {
-        'x-amzn-requestid': 'request-1'
-      }
-    },
-    output: {
-      body: Buffer.from('{"results":[]}')
+  t.context.segment = {
+    id: 'segment-1',
+    transaction: {
+      id: 'tx-1',
+      traceId: 'trace-1'
     }
   }
 
-  t.context.invokeCommand = {
-    input: {
-      accept: 'application/json',
-      contentType: 'application/json',
-      modelId: 'amazon.titan-text-express-v1',
-      body: '{"foo":"foo"}'
+  t.context.bedrockResponse = {
+    headers: {
+      'x-amzn-requestid': 'request-1'
     }
+  }
+
+  t.context.bedrockCommand = {
+    modelId: 'model-1'
   }
 })
 
-tap.skip('create creates a new instance', async (t) => {
+tap.test('create creates a new instance', async (t) => {
   const event = new LlmEvent(t.context)
   t.ok(event)
+  t.match(event.id, /[a-z0-9]{7}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/)
+  t.equal(event.vendor, 'bedrock')
+  t.equal(event.ingest_source, 'Node')
+  t.equal(event.appName, 'test-app')
   t.equal(event.api_key_last_four_digits, '6789')
+  t.equal(event.span_id, 'segment-1')
+  t.equal(event.transaction_id, 'tx-1')
+  t.equal(event.trace_id, 'trace-1')
+  t.equal(event.request_id, 'request-1')
+  t.equal(event['response.model'], 'model-1')
+  t.equal(event['request.model'], 'model-1')
+  t.equal(event['request.max_tokens'], null)
+  t.equal(event.error, false)
 })
