e61bd2fb6d537991e1a24e8f581ee5d498208fd5
James Sumners <jsumners@newrelic.com>
2024-03-11 13:47:48 -0400

    address feedback

--- tests/unit/llm/stream-handler.tap.js
+++ tests/unit/llm/stream-handler.tap.js
@@ -118,6 +118,7 @@ tap.test('handles claude streams', async (t) => {
   t.equal(br.requestId, 'aws-req-1')
   t.equal(br.statusCode, 200)
   t.equal(br.inputTokenCount, 5)
+  t.equal(br.outputTokenCount, 10)
 })
 
 tap.test('handles cohere streams', async (t) => {
@@ -165,6 +166,8 @@ tap.test('handles cohere streams', async (t) => {
   t.equal(br.finishReason, 'done')
   t.equal(br.requestId, 'aws-req-1')
   t.equal(br.statusCode, 200)
+  t.equal(br.inputTokenCount, 5)
+  t.equal(br.outputTokenCount, 10)
 })
 
 tap.test('handles cohere embedding streams', async (t) => {
@@ -217,6 +220,8 @@ tap.test('handles cohere embedding streams', async (t) => {
   t.equal(br.finishReason, undefined)
   t.equal(br.requestId, 'aws-req-1')
   t.equal(br.statusCode, 200)
+  t.equal(br.inputTokenCount, 5)
+  t.equal(br.outputTokenCount, 10)
 })
 
 tap.test('handles llama2 streams', async (t) => {
@@ -257,6 +262,8 @@ tap.test('handles llama2 streams', async (t) => {
   t.equal(br.finishReason, 'done')
   t.equal(br.requestId, 'aws-req-1')
   t.equal(br.statusCode, 200)
+  t.equal(br.inputTokenCount, 5)
+  t.equal(br.outputTokenCount, 10)
 })
 
 tap.test('handles titan streams', async (t) => {
@@ -307,4 +314,6 @@ tap.test('handles titan streams', async (t) => {
   t.equal(br.finishReason, 'done')
   t.equal(br.requestId, 'aws-req-1')
   t.equal(br.statusCode, 200)
+  t.equal(br.inputTokenCount, 5)
+  t.equal(br.outputTokenCount, 10)
 })
